16418a31a19b2e7bbf46bfb56ecffff86e71a04b
MoAlhait <61259093+MoAlhait@users.noreply.github.com>
2020-04-19 12:18:09 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,26 +1,39 @@
-
 import tkinter as tk
 
+def mink():
+	print("manamejeff!!")
+
 class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+	def __init__(self, master=None):
+		super().__init__(master)
+		self.master = master
+		self.pack()
+		self.create_widgets()
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+	def create_widgets(self):
+		self.hi_there = tk.Button(self)
+		self.hi_there["text"] = "World of Gumpshy\n(Enter Now)"
+		self.hi_there["command"] = self.changeColor("red")
+		self.hi_there["bg"] = "red"
+		self.hi_there["fg"] = "black"
+		self.hi_there.pack(side="top")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+		self.blue = tk.Button(self, text="Blue", fg="black", bg="blue", 
+			command=self.changeColor("blue"))
+		self.blue.pack(side="top")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+		self.quit = tk.Button(self, text="QUIT", fg="black",
+			command=self.master.destroy)
+		self.quit.pack(side="bottom")
 
-root = tk.Tk()
-app = Application(master=root)
+	def changeColor(self, col):
+		print(col)
+		self.configure(bg=col)
+		# print(self.bg)
+
+root1 = tk.Tk()
+app = Application(master=root1)
 app.mainloop()
+
+ #2 buttons 1 red 1 blue 1 says red on it other says blue on it
+	#click the red back becomes red blue back becomes blue
