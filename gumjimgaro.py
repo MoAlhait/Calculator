7f9e89b7ad9bf478af043ff9bed415f26588df6b
MoAlhait <61259093+MoAlhait@users.noreply.github.com>
2020-05-02 16:55:03 -0700

    frame for calculator
    made a frame for the calculator

--- gumjimgaro.py
+++ gumjimgaro.py
@@ -1,12 +1,22 @@
 import tkinter as tk
 
-class Calculator(tk.Frame):
+class Calculator(tk.Canvas):
 	def __init__(self, master=None):
-		super().__init__(master, width = 345, height = 400,bg="pink")
+		width = 350
+		height = 400
+		# Initialize a frame with width 350, height 400 and the bg Pink
+		super().__init__(master, width = width, height = height,bg="pink")
+
 		self.master = master
 		self.pack()
-
-
+		# make the bg for the calculator
+		calcWidth = 200
+		calcHeight = 250
+		x1 = (width/2) - (calcWidth/2)
+		y1 = (height/2) - (calcHeight/2)
+		x2 = (width/2) + (calcWidth/2)
+		y2 = (height/2) + (calcHeight/2)
+		self.create_rectangle(x1,y1,x2,y2)
 
 
 
